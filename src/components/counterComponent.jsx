a03465f95c62bf88ae9aadb5b0c7af75c0cdb79d
Moein <mohammadmoein.moradi@gmail.com>
2020-11-22 19:53:36 +0330

    feat: add increment handler

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -10,12 +10,16 @@ class Counter extends Component {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
         <ul>
           {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
         </ul>
       </React.Fragment>
       );
+  }
+
+  handleIncrement = ()=>{
+    console.log('clicked...', this);
   }
 
   getBadgeClasses(){
