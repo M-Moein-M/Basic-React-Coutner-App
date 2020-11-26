005084cee2b7ad7b8d6f0c322e9844ee952df39e
Moein <mohammadmoein.moradi@gmail.com>
2020-11-26 20:16:30 +0330

    feat: add multiple components

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,34 +2,32 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count : 0,
-    tags: ['tag1', 'tag2', 'tag3']
+    value : this.props.value,
   }
 
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+        <div>
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        </div>
       </React.Fragment>
       );
   }
 
   handleIncrement = ()=>{
-    console.log('clicked...', this);
+    this.setState( {value:this.state.value + 1} );
   }
 
   getBadgeClasses(){
-    const {count} = this.state;
-    return count === 0 ? 'badge badge-primary m-3' : 'badge badge-warning m-3'
+    const {value} = this.state;
+    return value === 0 ? 'badge badge-primary m-3' : 'badge badge-warning m-3'
   }
 
   formatCount(){
-    const {count} = this.state
-    return count === 0 ? 'Zero' : count;
+    const {value} = this.state
+    return value === 0 ? 'Zero' : value;
   }
 
 }
